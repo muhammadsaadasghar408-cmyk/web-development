9ec6f9355bf58a1737b94971f6883275ba169b8b
muhammadsaadasghar408 <muhammadsaadasghar408@gmail.com>
2025-11-06 19:48:22 +0500

    React Router Setup + Usage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import Alert from './components/Alert';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import React, { useState } from 'react';
 
 
@@ -30,7 +32,8 @@ setTimeout(() => {
       
          document.body.style.backgroundColor = 'black';  
       document.body.style.color = 'white';   
-      showAlert("dark mode has been enable","success");   
+      showAlert("dark mode has been enable","success");
+      document.title='textuits-Darkmode';   
     }
     else{
       setMode('light');
@@ -38,36 +41,43 @@ setTimeout(() => {
       document.body.style.backgroundColor = 'white';    // white background
       document.body.style.color = 'black'; 
       showAlert("light mode has been enable","success");   
+      document.title='textuits-lightmode';   
     }
   }
   const greenMode= ()=>{
     if (green=== 'light') {
-      setGreenMode('light');
+      setGreenMode('green');
       // document.body.style.backgroundColor='dark';
       
-         document.body.style.backgroundColor = 'green';  
+      document.body.style.backgroundColor = 'green';  
       document.body.style.color = 'white';   
       showAlert("green mode has been enable","success");   
+      document.title='textuits-greenmode';   
     }
     else{
-      setGreenMode('green');
+      setGreenMode('light');
       // document.body.style.backgroundColor='white';
       document.body.style.backgroundColor = 'white';    // white background
       document.body.style.color = 'black'; 
       showAlert("light mode has been enable","success");   
+      document.title='textuits-lightmode';   
     }
   }
   return (
     <>
+    <Router>
 <Navbar title="textuits2" abouttext="About us" mode={mode} green={green} greenMode={greenMode} toggleMode={toggleMode} />
 {/* <Navbar /> */}
 <Alert  alert={alert}/>
 <div className='container my-3'>
-<Textform heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}
-/>
-<About mode={mode} />
+     <Routes>
+          <Route path="/" element={<Textform heading="Enter the text to analyze below" mode={mode} showAlert={showAlert}/>} />
+          <Route path="/about" element={<About mode={mode} />} />
+          {/* <Route path="/home" element={<Home />} /> */}
+        </Routes>
 
 </div>
+</Router>
 </>
   );
 }
