2208940e7810e1d076ba4189644ecb0b2906a40b
muhammadsaadasghar408 <muhammadsaadasghar408@gmail.com>
2025-11-19 10:42:44 +0500

    Fetching API Key from News API

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,9 +4,19 @@ import Newsitems from './Newsitems'
 export class News extends Component {
   render() {
     return (
-      <div>
-        this is news component
-        <Newsitems/>
+      <div className="container my-3">
+        <h1>News-Top Headline</h1>
+        <div className="row">
+        <div className="col-md-4">
+        <Newsitems title="myTitle" discription="my description" />
+        </div>
+        <div className="col-md-4">
+        <Newsitems title="myTitle" discription="my description" />
+        </div>
+       <div className="col-md-4">
+        <Newsitems title="myTitle" discription="my description" />
+        </div>
+      </div>
       </div>
     )
   }
--- src/Components/Newsitems.js
+++ src/Components/Newsitems.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitems extends Component {
   render() {
+    let { title, discription } = this.props;
     return (
       <div>
-        This is a Newsitems
+        <div className="card" style={{width: "18rem"}}>
+          <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{discription}</p>
+            <a href="/newsdetail" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newsitems
+export default Newsitems;
