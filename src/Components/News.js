4293423b0a77cb9f2a3687f07e5fcf591501d013
muhammadsaadasghar408 <muhammadsaadasghar408@gmail.com>
2025-11-24 18:46:08 +0500

    Adding Previous & Next Buttons to populate NewsItems

--- src/Components/News.js
+++ src/Components/News.js
@@ -17,9 +17,44 @@ export class News extends Component {
       let data= await fetch(url);
       let parsedata= await data.json();
       console.log(parsedata);
-      this.setState({articles:parsedata.articles})
-
+      this.setState({articles:parsedata.articles, totalResults:parsedata.totalResults})
+  
     }
+
+     handleprevclick=async ()=>{
+       console.log("previous")
+       let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8853cbd503014bacbcb2d732e7a43ebc&page=${this.state.page-1}&pagesize=20`;
+      let data= await fetch(url);
+      let parsedata= await data.json();
+      console.log(parsedata);
+   
+   this.setState({
+   page:this.state.page-1,
+   articles:parsedata.articles
+   })
+    }
+
+i
+
+    handlenextclick=async ()=>{
+    console.log("next")
+    if (this.state.page+1 > Math.ceil((this.state.totalResults/20)) ) {
+      
+    }
+    else{
+    
+           let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8853cbd503014bacbcb2d732e7a43ebc&page=${this.state.page+1}&pagesize=20`;
+          let data= await fetch(url);
+          let parsedata= await data.json();
+          console.log(parsedata);
+      
+    this.setState({
+      page:this.state.page+1,
+      articles:parsedata.articles
+    })
+    }
+  }
+
   render() {
     console.log("render")
     return (
@@ -28,13 +63,13 @@ export class News extends Component {
         <div className="row">
        { this.state.articles.map((element)=> {
         return <div className="col-md-4" key={element.url}>
-        <Newsitems  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,80):""} imageurl={element.urlToImage} newsurl={element.url} />
+        <Newsitems  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,80):""} imageurl={element.urlToImage?element.urlToImage:"https://fortune.com/img-assets/wp-content/uploads/2025/11/GettyImages-2213490750-e1763849852801.jpg?resize=1200,600"} newsurl={element.url} />
         </div>
        })} 
       </div>
-<div className="container mx-3">
-  <button className="btn btn-primary">previous</button>
-  <button className="btn btn-primary">Next</button>
+<div className="container d-flex justify-content-between">
+  <button disabled={this.state.page<=1} className="btn btn-primary" onClick={this.handleprevclick}>&larr; previous</button>
+  <button disabled={this.state.page > Math.ceil((this.state.totalResults/20)) } className="btn btn-primary"onClick={this.handlenextclick}>Next &rarr;</button>
 </div>
 
       </div>
