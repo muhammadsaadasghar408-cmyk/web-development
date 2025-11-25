36de57c9fc57ee96c4340c9a7bc78f976d9ad086
muhammadsaadasghar408 <muhammadsaadasghar408@gmail.com>
2025-11-25 09:21:38 +0500

    enable dark mode

--- src/App.js
+++ src/App.js
@@ -5,11 +5,32 @@ import Navbar from './Components/Navbar';
 import News from './Components/News';
 
 export default class App extends Component {
+   constructor() {
+    super();
+    this.state = {
+      mode: 'light'   // dark ya light mode
+    }
+  }
+
+  toggleMode = () => {
+    if (this.state.mode === 'light') {
+      this.setState({ mode: 'dark' });
+      document.body.style.backgroundColor = '#042743';
+      document.body.style.color='white'
+    } else {
+      this.setState({ mode: 'light' });
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color='black'
+    }
+  }
+
   render() {
     return (
+
+
       <div>
        
-        <Navbar/>
+        <Navbar  mode={this.state.mode} toggleMode={this.toggleMode} />
         <News/>
        
       
